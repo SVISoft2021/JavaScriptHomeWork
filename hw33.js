c421b069b0923dd66feb1d59636d66513e74545b
SVISoft2021 <svisoft2015@gmail.com>
2022-01-21 07:18:16 +0200

    Задание 3.3

---
+++ hw33.js
@@ -0,0 +1,17 @@
+/*
+Задача 3. Сделайте функцию `f`, которая отнимает от первого числа второе и делит на третье. Данная функция должна обязательно содержать проверку входных 
+параметров, потому что принимать она может только числа.
+*/
+
+function f(a, b, c) {
+    if (typeof a != "number" ||
+        typeof b != "number" ||
+        typeof c != "number") {
+            throw new Error("all parameters type should be a Number");
+    }
+  
+    return (a - b) / c;
+}
+
+console.log("Задание 3.3. Ответ: " + f(9, 3, 2)); // 3
+console.log("Задание 3.3. Ответ: " + f('s', 9, 3)); // Error: all parameters type should be a Number
