17b24e4659c3ac3f78a285c1dbef1d0a2dcf07c1
SVISoft2021 <svisoft2015@gmail.com>
2022-01-21 07:14:29 +0200

    Задание 3.2

---
+++ hw32.js
@@ -0,0 +1,25 @@
+/*
+Задача 2. Напишите функцию `f` которая возвращает сумму всех параметров. Количество параметров может быть любым. Данная функция должна обязательно содержать 
+проверку входных параметров, потому что принимать она может только числа.
+*/
+
+// Решение
+const f = function(value) {
+    var result = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'number') {
+            result += arguments[i];
+        } else {
+            throw new Error('all parameters should be a Number type');
+        }
+    }
+
+    return result;
+};
+
+
+console.log("Задание 3.2. Ответ: " + f(1, 2, 3)); // 6
+console.log("Задание 3.2. Ответ: " + f(1, 1, 1, 1, 1, 1, 1, 1)); // 8
+console.log("Задание 3.2. Ответ: " + f(1, 2, 's', 4)); // Error: all parameters should be a Number type
+
