064f4824d4cab9e0d1396a5d956240db5be79c51
SVISoft2021 <svisoft2015@gmail.com>
2022-01-27 21:03:34 +0200

    Задача 4.1.

---
+++ Lesson-4/hw41.js
@@ -0,0 +1,29 @@
+/**
+ * Задача 4.1.
+ * Создать имплементацию функции `forEach`, логика работы такая же как и у родного метода.
+ * Функция должна содержать проверки:
+ * - Первый параметр обязателен и может принимать только массив
+ * - Второй параметр обязателен и может принимать только функцию
+ */
+
+ function forEach(array, callback) {
+    if (!Array.isArray(array)) {
+        throw new TypeError(array + ' это не массив');
+    }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' это не функция');
+    }
+
+    for (var index = 0; index < array.length; index++) {
+        callback(array[index], index, array);
+    }
+}
+
+const arr = [1, 2, 3];
+forEach(arr, function (item, i, arr) {
+    console.log(`массив [${arr}]: элемент [${i}] = ${item} `);
+});
+
+//forEach(arr, 'Текст');
+//forEach('Текст', arr);
